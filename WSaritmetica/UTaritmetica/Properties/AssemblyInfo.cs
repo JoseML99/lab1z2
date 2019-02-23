0038d0f1d2075c9674eb1a41ddfc67865d6a6808
Jose Mario Lami <jmlami99@gmail.com>
2019-02-23 13:56:38 -0400

    Agregamos unit test

---
+++ WSaritmetica/UTaritmetica/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UTaritmetica")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UTaritmetica")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c60f5179-e269-4c2c-a712-41d13d9c36f7")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
--- WSaritmetica/WCFaritmetica/IsrvSuma.cs
+++ WSaritmetica/WCFaritmetica/IsrvSuma.cs
@@ -17,7 +17,7 @@ namespace WCFaritmetica
     }
 
     [DataContract]
-    public class clsSuma
+    public class clsSuma:baseResultado
     {
         [DataMember]
         public int numeroA { get; set; }
@@ -26,5 +26,11 @@ namespace WCFaritmetica
         [DataMember]
         public int resultado { get; set; }
 
+    }
+
+    public class baseResultado
+    {
+        public string mensaje { get; set; }
+        public string error { get; set; }
     }
 }
